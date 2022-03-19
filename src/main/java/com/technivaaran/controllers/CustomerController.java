db48735825f9040b08bc805473230f16eea2d5db
mhrupa <mhrupa@gmail.com>
2022-03-19 23:30:09 +0530

    create customer changes

--- src/main/java/com/technivaaran/controllers/CustomerController.java
+++ src/main/java/com/technivaaran/controllers/CustomerController.java
@@ -2,6 +2,12 @@ package com.technivaaran.controllers;
 
 import java.util.List;
 
+import com.technivaaran.AppUrlConstants;
+import com.technivaaran.dto.CustomerDto;
+import com.technivaaran.dto.OmsResponse;
+import com.technivaaran.entities.CustomerEntity;
+import com.technivaaran.services.CustomerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,17 +17,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.technivaaran.dto.CustomerDto;
-import com.technivaaran.dto.OmsResponse;
-import com.technivaaran.entities.CustomerEntity;
-import com.technivaaran.services.CustomerService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
 @Slf4j
+@RestController
+@RequestMapping(AppUrlConstants.BASE_URL)
 public class CustomerController {
 
 	@Autowired
--- src/main/java/com/technivaaran/entities/CustomerEntity.java
+++ src/main/java/com/technivaaran/entities/CustomerEntity.java
@@ -1,18 +1,15 @@
 package com.technivaaran.entities;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.hibernate.validator.constraints.Length;
 import org.springframework.lang.NonNull;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
